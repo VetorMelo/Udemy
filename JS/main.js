0391777538b626bddb3bb7b4dc1f14010110a001
Victor Melo <contatomelo2@gmail.com>
2023-01-15 22:01:36 -0300

    update main,js

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,14 @@
-let nomeVariavel = "Victor";
-console.log(nomeVariavel);
-nomeVariavel = "Melo";
-console.log(nomeVariavel);
+let numRodas = 4;
+
+switch(numRodas){
+    case 1:
+    case 2:
+    case 3:
+        console.log("R$ 1,50");
+        break;
+    case 4:
+        console.log("R$ 5,50");
+        break;
+    default:
+        console.log("R$ 10,50");
+}
