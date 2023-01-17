3d6d8c6ddf83d5dd9bba52ea7ee929c24c355e1c
Victor Melo <contatomelo2@gmail.com>
2023-01-16 22:10:53 -0300

    update

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,2 @@
-let numRodas = 4;
-
-switch(numRodas){
-    case 1:
-    case 2:
-    case 3:
-        console.log("R$ 1,50");
-        break;
-    case 4:
-        console.log("R$ 5,50");
-        break;
-    default:
-        console.log("R$ 10,50");
-}
+let servicos = document.getElementById('servicos');
+servicos.innerHTML = "nossa primeira transformação"
